8ddca4aa3dc6c8e77a7a4cda8b78dbc231305468
Jorge Duarte <sadujorge@gmail.com>
2023-10-27 00:15:45 -0700

    Added question score generation and quiz completion logic

--- Assets/scripts/app.js
+++ Assets/scripts/app.js
@@ -1,16 +1,45 @@
 "use strict";
 // Global Variables
-let timer = 400;
+let timer = 40;
 let timerP = $('.timer');
 timerP.text(`Time: ${timer}`);
 let questionNumber = 0;
+let savedScore;
 const questions = [
     {
-        question: 'knock knock',
-        correct: 'who is there?',
-        choice1: 'what?',
-        choice2: '****',
-        choice3: 'Come back later',
+        question: 'Which of the following is not a primitive data type in JavaScript?',
+        correct: 'Array',
+        choice1: 'String',
+        choice2: 'Boolean',
+        choice3: 'Number',
+    },
+    {
+        question: 'Which method can be used to add an item to the end of an array?',
+        correct: 'push()',
+        choice1: 'concat()',
+        choice2: 'join()',
+        choice3: 'pop()',
+    },
+    {
+        question: 'How can you log messages to the console in JavaScript?',
+        correct: 'console.log()',
+        choice1: 'System.out.print()',
+        choice2: 'print()',
+        choice3: 'log.console()',
+    },
+    {
+        question: 'Which of the following is a way to declare a variable that cannot be reassigned in JavaScript?',
+        correct: 'const',
+        choice1: 'var',
+        choice2: 'let',
+        choice3: 'function',
+    },
+    {
+        question: 'What does the `this` keyword refer to inside an object method in JavaScript?',
+        correct: 'The object itself',
+        choice1: 'The method itself',
+        choice2: 'The global window object',
+        choice3: 'The parent object',
     }
 ];
 // Global Functions
@@ -29,12 +58,15 @@ function endQuiz() {
     quizFinishedPage.removeClass('hidden');
 }
 function resetTimer() {
-    timer = 400;
+    timer = 40;
     timerP.text(`Time: ${timer}`);
+}
+function resetQuestionNumber() {
+    questionNumber = 0;
 }
 function saveScores() {
     const initials = initialsInput.val();
-    const score = timer;
+    const score = savedScore;
     const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
     highScores.push({ initials: initials, score: score });
     localStorage.setItem('highScores', JSON.stringify(highScores));
@@ -44,21 +76,31 @@ function displayHighScores() {
     const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
     highScores.sort((a, b) => b.score - a.score);
     highScores.forEach(score => {
-        scoresUl.append(`<li class="scoresLi">${score.initials} - ${score.score}</li>`);
+        if (typeof score.score !== 'number') {
+            scoresUl.append(`<li class="scoresLi">${score.initials} - DNF! </li>`);
+        }
+        else {
+            scoresUl.append(`<li class="scoresLi">${score.initials} - ${score.score}</li>`);
+        }
     });
 }
 function generateQuestions() {
-    const questionValue = questions[questionNumber].question;
-    const choice1Value = questions[questionNumber].choice1;
-    const choice2Value = questions[questionNumber].choice2;
-    const choice3Value = questions[questionNumber].choice3;
-    const correctValue = questions[questionNumber].correct;
-    questionCounter.text(`Question ${questionNumber + 1}`);
-    question.text(questionValue);
-    wrong1.text(choice1Value);
-    wrong2.text(choice2Value);
-    wrong3.text(choice3Value);
-    right.text(correctValue);
+    if (questionNumber < questions.length) {
+        const questionValue = questions[questionNumber].question;
+        const choice1Value = questions[questionNumber].choice1;
+        const choice2Value = questions[questionNumber].choice2;
+        const choice3Value = questions[questionNumber].choice3;
+        const correctValue = questions[questionNumber].correct;
+        questionCounter.text(`Question ${questionNumber + 1}`);
+        question.text(questionValue);
+        wrong1.text(choice1Value);
+        wrong2.text(choice2Value);
+        wrong3.text(choice3Value);
+        right.text(correctValue);
+    }
+    else {
+        return;
+    }
 }
 // Home Page
 const homePage = $('.homePage');
@@ -93,12 +135,21 @@ const quizPage = $('.quizPage');
 const questionCounter = $('.quizH2');
 const question = $('.quizP');
 const navigateHomeBtnQuiz = $('#navigateHomeBtnQuiz');
+const optionBtn = $('.optionBtn');
 const wrong1 = $('#wrong1');
 const wrong2 = $('#wrong2');
 const wrong3 = $('#wrong3');
 const right = $('#right');
 navigateHomeBtnQuiz.on('click', function () {
     location.reload();
+});
+optionBtn.on('click', function () {
+    questionNumber++;
+    generateQuestions();
+    if (questionNumber >= questions.length) {
+        savedScore = timer;
+        timer = 0;
+    }
 });
 // Quiz Finished Page
 const quizFinishedPage = $('.quizFinishedPage');
@@ -110,11 +161,13 @@ initialsBtn.on('click', function () {
     homePage.removeClass('hidden');
     saveScores();
     resetTimer();
+    resetQuestionNumber();
 });
 navigateHomeBtnFinish.on('click', function () {
     quizFinishedPage.addClass('hidden');
     homePage.removeClass('hidden');
     resetTimer();
+    resetQuestionNumber();
 });
 // On load
 // homePage.addClass('hidden')
--- app.ts
+++ app.ts
@@ -1,23 +1,60 @@
 // Global Variables
-let timer: number = 400;
+let timer: number = 40;
 let timerP: JQuery<HTMLElement> = $('.timer');
 timerP.text(`Time: ${timer}`);
 let questionNumber: number = 0; 
+let savedScore: number;
 
-const questions = [
+const questions: Question[] = [
     {
-        question: 'knock knock',
-        correct: 'who is there?',
-        choice1: 'what?',
-        choice2: '****',
-        choice3: 'Come back later',
+        question: 'Which of the following is not a primitive data type in JavaScript?',
+        correct: 'Array',
+        choice1: 'String',
+        choice2: 'Boolean',
+        choice3: 'Number',
+    },
+    {
+        question: 'Which method can be used to add an item to the end of an array?',
+        correct: 'push()',
+        choice1: 'concat()',
+        choice2: 'join()',
+        choice3: 'pop()',
+    },
+    {
+        question: 'How can you log messages to the console in JavaScript?',
+        correct: 'console.log()',
+        choice1: 'System.out.print()',
+        choice2: 'print()',
+        choice3: 'log.console()',
+    },
+    {
+        question: 'Which of the following is a way to declare a variable that cannot be reassigned in JavaScript?',
+        correct: 'const',
+        choice1: 'var',
+        choice2: 'let',
+        choice3: 'function',
+    },
+    {
+        question: 'What does the `this` keyword refer to inside an object method in JavaScript?',
+        correct: 'The object itself',
+        choice1: 'The method itself',
+        choice2: 'The global window object',
+        choice3: 'The parent object',
     }
-]
+];
 
 // Types
 type HighScore = {
     initials: string;
     score: number;
+};
+
+type Question = {
+    question: string;
+    correct: string;
+    choice1: string;
+    choice2: string;
+    choice3: string;
 };
 // Global Functions
 function startTimer(): void {
@@ -39,13 +76,17 @@ function endQuiz(): void {
 }
 
 function resetTimer(): void {
-    timer = 400;
+    timer = 40;
     timerP.text(`Time: ${timer}`);
+}
+
+function resetQuestionNumber(): void {
+    questionNumber = 0;
 }
 
 function saveScores(): void {
     const initials: string  = initialsInput.val() as string;
-    const score: number = timer;
+    const score: number = savedScore;
     const highScores: HighScore[] = JSON.parse(localStorage.getItem('highScores') || '[]');
 
     highScores.push({ initials: initials, score: score });
@@ -60,22 +101,30 @@ function displayHighScores(): void {
     highScores.sort((a, b) => b.score - a.score);
 
     highScores.forEach(score => {
-        scoresUl.append(`<li class="scoresLi">${score.initials} - ${score.score}</li>`);
+        if (typeof score.score !== 'number') {
+            scoresUl.append(`<li class="scoresLi">${score.initials} - DNF! </li>`);
+        } else {
+            scoresUl.append(`<li class="scoresLi">${score.initials} - ${score.score}</li>`);
+        }
     });
 }
 
 function generateQuestions(): void {
-    const questionValue: string = questions[questionNumber].question;
-    const choice1Value: string = questions[questionNumber].choice1;
-    const choice2Value: string = questions[questionNumber].choice2;
-    const choice3Value: string = questions[questionNumber].choice3;
-    const correctValue: string = questions[questionNumber].correct;
-    questionCounter.text(`Question ${questionNumber + 1}`)
-    question.text(questionValue);
-    wrong1.text(choice1Value);
-    wrong2.text(choice2Value);
-    wrong3.text(choice3Value);
-    right.text(correctValue);
+    if (questionNumber < questions.length) {
+        const questionValue: string = questions[questionNumber].question;
+        const choice1Value: string = questions[questionNumber].choice1;
+        const choice2Value: string = questions[questionNumber].choice2;
+        const choice3Value: string = questions[questionNumber].choice3;
+        const correctValue: string = questions[questionNumber].correct;
+        questionCounter.text(`Question ${questionNumber + 1}`);
+        question.text(questionValue);
+        wrong1.text(choice1Value);
+        wrong2.text(choice2Value);
+        wrong3.text(choice3Value);
+        right.text(correctValue);
+    } else {
+        return;
+    }
 }
 // Home Page
 const homePage: JQuery<HTMLElement> = $('.homePage');
@@ -115,6 +164,7 @@ const quizPage: JQuery<HTMLElement> = $('.quizPage');
 const questionCounter: JQuery<HTMLElement> = $('.quizH2');
 const question: JQuery<HTMLParagraphElement> = $('.quizP');
 const navigateHomeBtnQuiz: JQuery<HTMLElement> = $('#navigateHomeBtnQuiz');
+const optionBtn: JQuery<HTMLButtonElement> = $('.optionBtn');
 const wrong1: JQuery<HTMLElement> = $('#wrong1');
 const wrong2: JQuery<HTMLElement> = $('#wrong2');
 const wrong3: JQuery<HTMLElement> = $('#wrong3');
@@ -122,6 +172,16 @@ const right: JQuery<HTMLElement> = $('#right');
 
 navigateHomeBtnQuiz.on('click', function(): void {
     location.reload();
+});
+
+optionBtn.on('click', function(): void {
+    questionNumber++;
+    generateQuestions();
+
+    if (questionNumber >= questions.length) {
+        savedScore = timer;
+        timer = 0;  
+    }
 });
 // Quiz Finished Page
 const quizFinishedPage: JQuery<HTMLElement> = $('.quizFinishedPage');
@@ -134,12 +194,14 @@ initialsBtn.on('click', function(): void {
     homePage.removeClass('hidden');
     saveScores();
     resetTimer();
+    resetQuestionNumber();
 });
 
 navigateHomeBtnFinish.on('click', function(): void {
     quizFinishedPage.addClass('hidden');
     homePage.removeClass('hidden');
     resetTimer();
+    resetQuestionNumber();
 });
 // On load
 // homePage.addClass('hidden')
